36b43e525078e01584daafbb845fd95f6a470652
Vedant <vedantdumbre0706@gmail.com>
2026-03-02 20:23:39 +0530

    Made a Video calling app named Huddle

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const protectedRoutes = createRouteMatcher([
+  "/",
+  "/upcoming",
+  "/meeting(.*)",
+  "/previous",
+  "/recordings",
+  "/personal-room",
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (protectedRoutes(req)) {
+    const { userId, redirectToSignIn } = await auth();
+
+    if (!userId) {
+      return redirectToSignIn();
+    }
+  }
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
